3e72de4a61e2cf1990c79e5b0ddea2ffff173d2e
SalehMAljohi <Salehaljohi33@gmail.com>
2025-03-17 14:08:36 +0300

    Add project files.

---
+++ RepositoryPatternWithUnitOfWork.EF/DA/ApplicationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RepositoryPatternWithUnitOfWork.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryPatternWithUnitOfWork.EF.DA
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options):base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ WebRepositoryPattern/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebRepositoryPattern.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
